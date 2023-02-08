7d98af3d4f0592bc51f4a2497fbf1d31618d77f7
Spiros <kmaris1996@gmail.com>
2023-02-09 01:37:20 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from './services/supabase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'uniTalks';
+export default class AppComponent implements OnInit {
+  constructor(private supabase: SupabaseService) {}
+  ngOnInit(): void {
+    this.supabase.getCards();
+    console.log(this.supabase.getCards().then(() => {}));
+  }
+  title = 'Write your story | uniTalks';
 }
